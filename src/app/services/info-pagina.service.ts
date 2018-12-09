c459535f4f65850502018c5d9359384cd33812fa
Jhon Contreras <jhon.contreras13@gmail.com>
2018-12-09 11:47:46 -0300

    Fin Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[];
   // dclaramos private http e importamos HttpClient para realizar peticiones al archivo JSON
   constructor( private http: HttpClient) {
-    // console.log('Servicio de infoPagina listo');
 
-    // LEER ARCHIVO JSON
-    this.http.get('assets/data/data-pagina.json')
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo(){
+      // LEER ARCHIVO JSON
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp:InfoPagina) =>{
         this.cargada = true;
         this.info = resp;
+        
+      
+      });
+   }
+
+   private cargarEquipo(){
+      // LEER FIREBASE
+      this.http.get('https://angular-html-ab23f.firebaseio.com/equipo.json')
+      .subscribe( (resp:any[]) =>{
+        this.equipo = resp;
+       
         console.log(resp);
       
-    });
-
+      });
    }
 }
